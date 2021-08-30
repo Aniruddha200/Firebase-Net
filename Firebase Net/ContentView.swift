90bb40c830b5a0f6ac946495aa501b3c761d642e
Aniruddha <aniruddha200baidya@protonmail.com>
2021-08-30 17:08:57 +0100

    Authentication is implemented.

--- Firebase Net/ContentView.swift
+++ Firebase Net/ContentView.swift
@@ -6,11 +6,60 @@
 //
 
 import SwiftUI
+import FirebaseAuth
 
 struct ContentView: View {
+	@State private var username = ""
+	@State private var password = ""
+	@State private var isVerifiedAlertShowing = false
+	@State private var isUnverifiedAlertShowing = false
+	
+	var auth = Auth.auth()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+		VStack{
+		TextField("Username", text: $username)
+		TextField("Password", text: $password)
+		}
+		.autocapitalization(.none)
+		.textFieldStyle(RoundedBorderTextFieldStyle())
+		.padding()
+		Button(action: {auth.signIn(withEmail: username, password: password) { _, error in
+			if error == nil{
+				isVerifiedAlertShowing.toggle()
+			}
+			else{
+				isUnverifiedAlertShowing.toggle()
+				
+				print(error?.localizedDescription ?? "Unknown Error")
+			}
+		}}){
+			Text("SignIn")
+				.frame(width: 100, height: 50, alignment: .center)
+				.background(Color.red)
+				.foregroundColor(.white)
+				.clipShape(Capsule())
+		}
+		.padding(.vertical
+		)
+		.alert(isPresented: $isVerifiedAlertShowing){
+			Alert(title: Text("SignIn Successful"))
+		}
+		
+		
+		Button(action:{do{
+			try auth.signOut()}
+		catch{
+			print("Failed to signout")
+		}}){
+			Text("Sign Out")
+				.frame(width: 100, height: 50, alignment: .center)
+				.background(Color.red)
+				.foregroundColor(.white)
+				.clipShape(Capsule())
+		}
+		.alert(isPresented: $isUnverifiedAlertShowing){
+			Alert(title: Text("SignIn Failed"))
+		}
     }
 }
 
--- Firebase Net/Firebase_NetApp.swift
+++ Firebase Net/Firebase_NetApp.swift
@@ -6,9 +6,13 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct Firebase_NetApp: App {
+	init() {
+		FirebaseApp.configure()
+	}
     var body: some Scene {
         WindowGroup {
             ContentView()
