4724c794d9095692d02142f99789d62d5fea50be
Aniruddha <aniruddha200baidya@protonmail.com>
2021-08-30 14:49:26 +0100

    Initial Commit

---
+++ Firebase Net/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Firebase Net
+//
+//  Created by administrator on 30/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Firebase Net/Firebase_NetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_NetApp.swift
+//  Firebase Net
+//
+//  Created by administrator on 30/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_NetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
